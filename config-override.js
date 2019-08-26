9afe38b5d183d0e1b8f820a61da4f3083dcc76d3
Rodrigo Mesquita <rodrigoriome@gmail.com>
2019-08-26 04:50:12 -0300

    delete trash file

--- config-override.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  webpack: config => {
-    config.module.rules[2].oneOf[5].use[2].options = {};
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
-    return config;
-  }
-};
