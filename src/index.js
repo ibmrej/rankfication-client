35f3d2a9337d76722e5e4679dc694e8fed5cc793
Rodrigo Mesquita <rodrigoriome@gmail.com>
2019-08-26 04:48:01 -0300

    add env config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import dotenv from "dotenv";
 import Root from "@/components/Root";
 import "@/styles.scss";
+
+dotenv.config();
 
 ReactDOM.render(<Root />, document.getElementById("root"));
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === "production" ? process.env.API_URL : "http://localhost:9000/"
+  baseURL: process.env.API_URL
 });
 
 export default api;
